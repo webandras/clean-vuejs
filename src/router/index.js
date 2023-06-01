31d1d2f6fb3210cfef898091e9f3902e58fca620
Gulácsi András <guland@protonmail.com>
2023-06-02 01:05:45 +0200

    fix: Use auth store in router file, rest url at login, delete old state.js file

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Hello from "../pages/private/Hello.vue";
 import Login from "../pages/public/Login.vue";
 import Dashboard from "../pages/private/Dashboard.vue";
 
-import {isAuthenticated, setState} from "../state/state";
+import {authStore} from "../store/authStore";
 
 const routes = [
     {
@@ -42,17 +42,17 @@ const router = createRouter({
 router.beforeEach(async (to, from) => {
     if (
         // make sure the user is authenticated
-        !isAuthenticated() &&
+        !authStore.isAuthenticated() &&
         // ❗️ Avoid an infinite redirect
         to.name !== 'Login'
     ) {
-        setState('message', 'You need to login to access this page.');
+        authStore.message = 'You need to login to access this page.'
         // redirect the user to the login page
         return {name: 'Login'}
     }
 
     // when logged in redirect from login page to home
-    if (isAuthenticated() && to.name === 'Login') {
+    if (authStore.isAuthenticated() && to.name === 'Login') {
         // redirect the logged-in user to the home page
         return {name: 'Dashboard'}
     }
